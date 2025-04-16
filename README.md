81b411e5e40a62226e783d88effedf152a3955f5
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-04-16 16:41:18 -0300

    feat
    - API quase pronta;
    
    - Precisa fazer ainda:
         - Melhorar o código no geral, e entender melhor como funciona

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-# API-Trancricao-Audio
+# API para transcrição de áudio
 
-```python
-pip install requests
+Para instalar a biblioteca de transcrição de áudio:
+```bash
+    pip install git+https://github.com/openai/whisper.git
+```
+
+Para instalar a biblioteca que lida com as requisições HTTP (aceitando arquivos):
+```bash
+    pip install fastapi uvicorn python-multipart
+```
+
+Uvicorn serve para executar o servidor com FastAPI e executar as alterações rapidamente para teste
+```bash
+    pip install uvicorn
+```
+
+
+Para rodar a API (somente após instalar as dependencias com pip):
+```bash
+    uvicorn main:app --reload
 ```
 
 ### OBS: todos os pacotes instalados pelo pip, ficam no .venv da pasta raiz do projeto (isso só se estiver usando arch linux, se não provavelmente fica no global)
--- main.py
+++ main.py
@@ -1,5 +1,40 @@
-import requests
+import whisper
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import tempfile
+import os
 
-response = requests.get("https://api.github.com")
-print("Status:", response.status_code)
-print("Content-Type:", response.headers["Content-Type"])
+app = FastAPI()
+model = whisper.load_model("base")
+
+# Verificar a extensão do arquivo
+def checar_extensao_arquivo(filename: str):
+    extensoes_validas = ['.mp3', '.wav', '.ogg']
+
+    # recupera a extensão do arquivo
+    extensao = os.path.splitext(filename)[1]
+
+    if extensao.lower() not in extensoes_validas:
+        raise HTTPException(status_code=400, detail=f"Formato de arquivo inválido: {extensao}")
+    return extensao
+
+# Cria a função para receber o áudio (em "file: UploadFile" recebe, e em "File(...)" diz que o arquivo é obrigatório) 
+@app.post("/transcrever/")
+async def upload_audio(file: UploadFile = File(...)):
+    checar_extensao_arquivo(file.filename)
+
+    # Armazena os bytes do áudio
+    audio = await file.read();
+
+    # Salva um arquivo temporário do áudio para o whisper poder transcrever:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp:
+        tmp.write(audio)
+        temp_audio = tmp.name
+    
+    # Transcrever áudio para texto
+    resultado = model.transcribe(temp_audio, language="pt")
+
+    # Retorna o body da requisição
+    return {
+        "transcricao": resultado["text"]
+    }
+    
